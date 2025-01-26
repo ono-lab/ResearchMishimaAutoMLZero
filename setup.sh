7fcfb933e5f563f690d9090b6295086efbf928ef
三嶋 隆史 <ryuji.mishima.com@gmail.com>
2025-01-27 02:46:54 +0900

    chore: update setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y openjdk-17-jdk
 sudo apt install -y maven
-
-sudo sh -c 'echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> /etc/environment'
-source /etc/environment
+sudo apt install -y make
